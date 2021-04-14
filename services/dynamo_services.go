52836a028c18577ec5033fc62a8cc93a0f5adf10
tk <kepes.tomas@gmail.com>
2021-04-14 21:04:15 +0200

    refactor

--- services/dynamo_services.go
+++ services/dynamo_services.go
@@ -1,4 +1,4 @@
-package move_util
+package services
 
 import (
 	"fmt"
@@ -8,6 +8,14 @@ import (
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 )
+
+type RMS struct {
+	DeviceId int     `json:"device_id"`
+	Time     int     `json:"time"`
+	Acc_x    float64 `json:"acc_x_rms"`
+	Acc_y    float64 `json:"acc_y_rms"`
+	Acc_z    float64 `json:"acc_z_rms"`
+}
 
 type Move struct {
 	DeviceId int `json:"device_id"`
@@ -20,6 +28,22 @@ func AddMoveDB(move Move, svc *dynamodb.DynamoDB) {
 	input := &dynamodb.PutItemInput{
 		Item:      av,
 		TableName: aws.String("tinyml-move"),
+	}
+	if err != nil {
+		fmt.Println("Error with marshalling.")
+	}
+
+	_, err = svc.PutItem(input)
+	if err != nil {
+		log.Fatalf("Got error calling PutItem: %s", err)
+	}
+}
+
+func AddRMSDB(rms RMS, svc *dynamodb.DynamoDB) {
+	av, err := dynamodbattribute.MarshalMap(rms)
+	input := &dynamodb.PutItemInput{
+		Item:      av,
+		TableName: aws.String("tinyml-rms"),
 	}
 	if err != nil {
 		fmt.Println("Error with marshalling.")
