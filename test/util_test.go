6faef83ed97b656134d4dcbdfee55780b73ba749
tk <kepes.tomas@gmail.com>
2021-04-18 12:14:41 +0200

    basic auth unit test

---
+++ test/util_test.go
@@ -0,0 +1,28 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/tkeyo/tinyml-be/util"
+)
+
+func TestIsAuthorized(t *testing.T) {
+	type TestCase struct {
+		input    string
+		expected bool
+	}
+
+	cases := []TestCase{
+		{"000", false},
+		{"123", true},
+	}
+
+	for _, c := range cases {
+		actual := util.IsAuthorized(c.input)
+		expected := c.expected
+
+		if actual != expected {
+			t.Fatalf("Expected %t, got %t", expected, actual)
+		}
+	}
+}
